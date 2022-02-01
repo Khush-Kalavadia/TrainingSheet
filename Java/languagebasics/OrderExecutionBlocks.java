312f8dc7d39bcccf7e796aa9e035b58c360d7206
Khush-Kalavadia <56917476+Khush-Kalavadia@users.noreply.github.com>
2022-02-01 14:24:04 +0530

    Add files via upload

---
+++ Java/languagebasics/OrderExecutionBlocks.java
@@ -0,0 +1,39 @@
+package com.java.languagebasics;
+
+public class OrderExecutionBlocks {
+
+    //constructor
+    OrderExecutionBlocks()
+    {
+        System.out.println("Constructor block");
+    }
+
+    //this is instance initializer block
+    {
+        System.out.println("IIB block");
+    }
+
+    static
+    {
+        System.out.println("Static block");
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            System.out.println("Working in main\n");
+
+            OrderExecutionBlocks object1 = new OrderExecutionBlocks();
+
+            OrderExecutionBlocks object2 = new OrderExecutionBlocks();
+
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+    }
+
+}
