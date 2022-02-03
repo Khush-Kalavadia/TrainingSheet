b68d887ca061bb8f82bc89708d69e16227c25a24
Khush-Kalavadia <56917476+Khush-Kalavadia@users.noreply.github.com>
2022-02-03 19:55:26 +0530

    Add files via upload

---
+++ Java/languagebasics/WildcardsGeneric.java
@@ -0,0 +1,35 @@
+package com.java.languagebasics;
+
+class Host<T>{}
+
+class Network
+{
+    public void add(Host<?> h1){}       //can't mention Host<T> so ? which is unbounded wildcard
+}
+
+public class WildcardsGeneric
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Network n1 = new Network();
+
+            n1.add(new Host<Object>());
+
+//            n1.add(new Host<Integer>());    //any object works
+
+//            n1.add(new Host<String>());     //any object works
+
+            Host<?> newHost;                //can also be used in declaration
+
+//            Host myHost = new Host<?>();    //over here you can't use wildcard
+
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+    }
+}
